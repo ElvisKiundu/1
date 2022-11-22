bdb7b35c30102c66e7d85b085152d03b71ccd0a1
Montache <Elvisnjeru1296@gmail.com>
2022-11-22 11:53:10 +0300

    Add files via upload

---
+++ Blog/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('', views.bloghome, name='bloghome'),
+    path('Finding_a_new_home', views.Finding_a_new_home, name='Finding_a_new_home'),
+    path('', views.bloghome, name='bloghome'),
+    path('', views.bloghome, name='bloghome'),
+    path('', views.bloghome, name='bloghome'),
+    path('', views.bloghome, name='bloghome'),
+    path('', views.bloghome, name='bloghome'),
+    path('', views.bloghome, name='bloghome'),
+    path('', views.bloghome, name='bloghome'),
+    path('', views.bloghome, name='bloghome'),
+    path('', views.bloghome, name='bloghome'),
+    path('', views.bloghome, name='bloghome'),
+    path('', views.bloghome, name='bloghome'),
+    path('', views.bloghome, name='bloghome'),
+    path('', views.bloghome, name='bloghome'),
+    path('', views.bloghome, name='bloghome'),
+]
---
+++ Blog/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bloghome(request):
+    return render(request, 'Blog/bloghome.html', {
+
+    })
+
+def Finding_a_new_home(request):
+    return render(request, 'Blog/Finding a new home.html', {
+
+    })
+
+def bloghome(request):
+    return render(request, 'Blog/bloghome.html', {
+
+    })
+
+def bloghome(request):
+    return render(request, 'Blog/bloghome.html', {
+
+    })
+
+def bloghome(request):
+    return render(request, 'Blog/bloghome.html', {
+
+    })
+
+def bloghome(request):
+    return render(request, 'Blog/bloghome.html', {
+
+    })
+
+def bloghome(request):
+    return render(request, 'Blog/bloghome.html', {
+
+    })
+
+def bloghome(request):
+    return render(request, 'Blog/bloghome.html', {
+
+    })
+
+def bloghome(request):
+    return render(request, 'Blog/bloghome.html', {
+
+    })
+
+def bloghome(request):
+    return render(request, 'Blog/bloghome.html', {
+
+    })
+
+def bloghome(request):
+    return render(request, 'Blog/bloghome.html', {
+
+    })
+
+def bloghome(request):
+    return render(request, 'Blog/bloghome.html', {
+
+    })
+
+def bloghome(request):
+    return render(request, 'Blog/bloghome.html', {
+
+    })
+
+def bloghome(request):
+    return render(request, 'Blog/bloghome.html', {
+
+    })
+
+def bloghome(request):
+    return render(request, 'Blog/bloghome.html', {
+
+    })
+
+def bloghome(request):
+    return render(request, 'Blog/bloghome.html', {
+
+    })
